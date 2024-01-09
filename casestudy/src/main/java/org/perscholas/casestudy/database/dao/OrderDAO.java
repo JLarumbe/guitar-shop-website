cb0257806c0fc2e65891203958d7e946955caca5
Jorge Larumbe <jorge.larumbem@gmail.com>
2024-01-09 11:41:09 -0500

    Ceated Junit tests for OrderDAO and UserDao

--- casestudy/src/main/java/org/perscholas/casestudy/database/dao/OrderDAO.java
+++ casestudy/src/main/java/org/perscholas/casestudy/database/dao/OrderDAO.java
@@ -1,8 +1,10 @@
 package org.perscholas.casestudy.database.dao;
 
+import jakarta.transaction.Transactional;
 import org.perscholas.casestudy.database.entity.Order;
 import org.perscholas.casestudy.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -15,4 +17,9 @@ public interface OrderDAO extends JpaRepository<Order, Long> {
 
     @Query("SELECT o FROM Order o WHERE o.user.id = :id AND o.status = 'CART'")
     Order findByUser(int id);
+
+    //Testing
+    @Modifying
+    @Transactional
+    int deleteById(Integer id);
 }
